48ca04c5065a04c064dd7a49d167b7a218f94fe9
Derli Machado <dam.machado1@gmail.com>
2020-09-28 14:18:33 -0300

    Add: structure of the signin page completed.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,46 +1,66 @@
 import React from 'react';
-import {Image, View} from 'react-native';
+import {Image, ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
-import { Container, Title, ForgotPassword, ForgotPasswordText } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={ {flex: 1}}
+      >
+      <Container>
+        <Image source={logoImg} />
 
-        <Title>Faça seu login</Title>
-          <Input
-          name="E-mail"
-          icon="mail"
-          placeholder="E-mail"
+          <Title>Faça seu login</Title>
+            <Input
+            name="E-mail"
+            icon="mail"
+            placeholder="E-mail"
 
-          />
-          <Input
-            name="password"
-            icon="lock"
-            placeholder="Senha"
-          />
-          <Button
-            onPress={() => {
-              console.log("Foi")
-            }}
-          >
-            Entrar
-          </Button>
+            />
+            <Input
+              name="password"
+              icon="lock"
+              placeholder="Senha"
+            />
+            <Button
+              onPress={() => {
+                console.log("Foi")
+              }}
+            >
+              Entrar
+            </Button>
 
-          <ForgotPassword
-            onPress={() => {
-              console.log('Foi');
-            }}
-          >
-            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-          </ForgotPassword>
+            <ForgotPassword
+              onPress={() => {
+                console.log('Foi');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
 
 
-    </Container>
+      </Container>
+    </ScrollView>
+
+    <CreateAccountButton onPress={ () => {}}>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+    </CreateAccountButton>
+    </>
   );
 };
 
